69074fe09e527222e96bff0517a5b3fb689be68a
“Xskypi0” <xskypi0@gmail.com>
2025-04-28 20:09:45 -0600

    feat: add menu items and display in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import { menuItems } from "./data/db"
+import MenuItem from "./components/MenuItem"
 
 function App() {
 
@@ -7,6 +8,22 @@ function App() {
       <header className=" bg-teal-500 py-5">
         <h1 className="text-center text-white text-4xl font-black">Calculadora de propinas y consumo</h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+          {
+            menuItems.map((item) => (
+              <MenuItem 
+              key={item.id}
+              
+              />
+            ))
+          }
+        </div>
+        <div>
+          <h2>consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
