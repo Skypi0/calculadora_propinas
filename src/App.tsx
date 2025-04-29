ece969ca7991da2b747080deceabd6ad4631efb0
“Xskypi0” <xskypi0@gmail.com>
2025-04-28 22:23:41 -0600

    feat: enhance MenuItem component to display item details

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,22 @@ function App() {
       <header className=" bg-teal-500 py-5">
         <h1 className="text-center text-white text-4xl font-black">Calculadora de propinas y consumo</h1>
       </header>
-      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menu</h2>
+      <main className="bg-white max-w-7xl mx-auto py-20 grid md:grid-cols-2 md:p-10">
+        <div className="p-5">
+          <h2 className="text-5xl font-black pb-5">Menu</h2>
+          <div className="space-y-5">
           {
             menuItems.map((item) => (
               <MenuItem 
               key={item.id}
-              
+              item={item}
               />
             ))
           }
+          </div>
         </div>
         <div>
-          <h2>consumo</h2>
+          <h2 className="text-5xl font-black pb-5">Consumo</h2>
         </div>
       </main>
     </>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,17 @@
-export default function MenuItem() {
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+  // Define the props for the MenuItem component here
+  item: MenuItem
+}
+export default function MenuItem({item}: MenuItemProps) {
+  // Destructure the item prop to get the properties you need
   return (
-    <div>MenuItem</div>
+    <>
+    <button className="flex w-10/12 items-center justify-between bg-gray-100 p-5 rounded-lg shadow-md hover:bg-gray-200 transition duration-300 border-2 border-gray-300">
+    <p>{item.name}</p>
+    <p className="font-black">{item.price}</p>
+    </button>
+    </>
   )
 }
